9f3cd24c805d47ecbfcff53d2c7c65d80ff26571
mygitname <vijaychendage55@gmail.com>
2024-08-03 21:37:04 +0530

    sample java programme for test jenkins

---
+++ Jenkin_github_test_class.java
@@ -0,0 +1,11 @@
+package jenkin_github_test_package;
+
+public class Jenkin_github_test_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*************************************");
+		System.out.println("Sample java class for github & jenkin");
+		System.out.println("*************************************");
+	}
+}
